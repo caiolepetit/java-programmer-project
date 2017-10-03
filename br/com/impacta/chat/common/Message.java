d0ae9bbf3c0c9311afabbe18a18f279fd22849c5
no0509@CLASSROOM.NET <no0509@CLASSROOM.NET>
2017-10-02 21:09:56 -0300

    Class Message adicionada

---
+++ br/com/impacta/chat/common/Message.java
@@ -0,0 +1,33 @@
+package br.com.impacta.chat.common;
+
+public class Message implements Protocol {
+
+	public Message (final int ipAddress, final String nickName){
+		
+	}
+	
+	@Override
+	public long getTimestamp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getClienteIP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getNickName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
