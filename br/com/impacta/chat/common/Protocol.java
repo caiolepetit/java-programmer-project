293790e192ecaeed83bef60089bc1e33e835f62c
no0509@CLASSROOM.NET <no0509@CLASSROOM.NET>
2017-10-02 21:05:32 -0300

    Adicionado os métodos nas interfaces

--- br/com/impacta/chat/common/Protocol.java
+++ br/com/impacta/chat/common/Protocol.java
@@ -2,8 +2,11 @@ package br.com.impacta.chat.common;
 
 public interface Protocol {
 
-		public long getTimestamp();
-		public int getClienteIP();
-		public String getNickName();
-		public String getMessage();
+	public long getTimestamp();
+
+	public int getClienteIP();
+
+	public String getNickName();
+
+	public String getMessage();
 }
--- br/com/impacta/chat/common/Sendable.java
+++ br/com/impacta/chat/common/Sendable.java
@@ -2,4 +2,7 @@ package br.com.impacta.chat.common;
 
 public interface Sendable {
 
+	public byte[] getBytes();
+
+	public Protocol fromBytes(byte[] bytes);
 }
